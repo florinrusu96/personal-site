4dc4bf0b13dfd6b97337caad144c29afead3f863
florinrusu96 <rusuflorinandrei96@gmail.com>
2020-07-01 06:51:07 -0700

    Contanct form

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function sendMail(){
+    var toEmail = document.getElementById('email').value;
+    var subject = document.getElementById('title').value;
+    var message = document.getElementById('subject').value;
+    
+    if (toEmail === "" || message === "" || subject === ""){
+        return
+    }
+
+}
